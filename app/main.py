c8aa5529c82dfc243d165366ea9e0003c571f739
jhass <s25132@pjwstk.edu.pl>
2025-05-10 17:32:05 +0200

    Dodanie prostej walidacji.

--- app/main.py
+++ app/main.py
@@ -31,10 +31,21 @@ def check_auth(credentials: HTTPBasicCredentials = Depends(security)):
             headers={"WWW-Authenticate": "Basic"},
         )
 
+def validate_non_empty_strings(data):
+    for field_name, value in data:
+        if isinstance(value, str) and value.strip() == "":
+            raise HTTPException(
+                status_code=400,
+                detail=f"Pole '{field_name}' nie może być puste."
+            )
+        
 @app.post("/predict")
 def predict(input_data: PredictionInput, _: str = Depends(check_auth)):
     if predictor is None:
         return {"status": "EMPTY PREDICTOR"}
+    
+    
+    validate_non_empty_strings(input_data)
 
     try:
         input_df = pd.DataFrame([input_data.model_dump()])
